082bd75f57af9fda80717f574043b29407e749e9
Opeyemi Oduyemi <119660096+oduyemi@users.noreply.github.com>
2025-06-26 19:29:21 +0100

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import "animate.css";
-import { Header } from "@/components/Header";
 import ClientSideLayout from "@/components/ClientSideLayout"; 
 
 export const metadata = {
