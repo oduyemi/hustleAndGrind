5dc3385292e8d55d9728ed65e0b2347693286ac7
Opeyemi Oduyemi <119660096+oduyemi@users.noreply.github.com>
2025-06-28 19:51:16 +0100

    Updated again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
       <Benefits />
       </div>
       <Selection />
+      <Core />
       <div>
         <Application />
       </div>
-      <Core />
     </div>
   );
 }
